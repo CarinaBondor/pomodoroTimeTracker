4f08e3e1af2a7a4a7e898eb2b1ab99a5f5fa4611
Bondor-Szekely Lavinia Carina <carinabondor@gmail.com>
2026-08-15 18:24:43 +0300

    alignment

--- src/components/ButtonsControl.tsx
+++ src/components/ButtonsControl.tsx
@@ -1,4 +1,7 @@
 import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import { ButtonsControlProps } from "../types";
 
 function ButtonsControl({
@@ -8,26 +11,65 @@ function ButtonsControl({
     isRunning,
 }: ButtonsControlProps): React.ReactElement {
     return (
-        <div>
-            <Button
+        <>
+            <ButtonGroup
                 variant="contained"
-                onClick={() => onStart()}
-                disabled={isRunning}
+                aria-label="Button group for controlling the Pomodoro timer"
+                sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
             >
-                Start
-            </Button>
-            <Button
-                variant="contained"
-                onClick={onStop}
-                disabled={!isRunning}
+                <Button
+                    variant="contained"
+                    onClick={() => onStart()}
+                    disabled={isRunning}
+                >
+                    Start
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={onStop}
+                    disabled={!isRunning}
+                >
+                    Pause
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={onReset}
+                >
+                    Reset
+                </Button>
+            </ButtonGroup>
+
+            <Grid
+                container
+                spacing={2}
+                sx={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
             >
-                Pause
-            </Button>
-            <button onClick={onReset}>Reset</button>
-            <button onClick={() => onStart(600)}>10 min</button>
-            <button onClick={() => onStart(900)}>15 min</button>
-            {/* <button onClick={() => onStart(300)}>5 min break</button> */}
-        </div>
+                <Button
+                    variant="outlined"
+                    onClick={() => onStart(600)}
+                >
+                    10 min
+                </Button>
+                <Button
+                    variant="outlined"
+                    onClick={() => onStart(900)}
+                >
+                    15 min
+                </Button>
+                {/* <Button
+                    variant="contained"
+                    onClick={() => onStart(300)}
+                >
+                    5 min break
+                </Button> */}
+            </Grid>
+        </>
     );
 }
 
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,10 +1,18 @@
 import { ClockProps } from "../types";
+import { Grid } from "@mui/material";
 
 function Clock({ formattedTime }: ClockProps): React.ReactElement {
     return (
-        <div>
+        <Grid
+            container
+            direction="row"
+            sx={{
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
             <h1>{formattedTime}</h1>
-        </div>
+        </Grid>
     );
 }
 
